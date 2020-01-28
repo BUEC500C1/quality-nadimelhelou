728baf52e37ac6df9e782ec2c7e66a42dd7e7d93
Nadim El Helou <45805207+nadimelhelou@users.noreply.github.com>
2020-01-27 23:08:15 -0500

    removed "Tests that will fail"

--- test_hw1.py
+++ test_hw1.py
@@ -9,8 +9,6 @@ def test_arabic_to_roman():
     assert hw1.arabic_to_roman(467) == "CDLXVII"
     assert hw1.arabic_to_roman(1999) == "MCMXCIX"
     assert hw1.arabic_to_roman(2.5) == ''
-
-    # Tests that will fail
-    assert hw1.arabic_to_roman('3') == "III"
-    assert hw1.arabic_to_roman(-1) == "I"
-    assert hw1.arabic_to_roman(2.5) == "II"
+    assert hw1.arabic_to_roman('3') == ''
+    assert hw1.arabic_to_roman(-1) == ''
+    assert hw1.arabic_to_roman(2.5) == ''
